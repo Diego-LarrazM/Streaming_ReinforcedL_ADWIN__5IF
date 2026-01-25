d278e83a96341d00e6b52208758dd9b94171dedd
howdrox <97843576+howdrox@users.noreply.github.com>
2026-01-25 13:52:09 +0100

    updated bucketing with additive stats

--- rladwin/src/main/java/if5/research/core/bucketing/mock/BucketTest.java
+++ rladwin/src/main/java/if5/research/core/bucketing/mock/BucketTest.java
@@ -1,4 +1,22 @@
 package if5.research.core.bucketing.mock;
 
+import if5.research.core.bucketing.model.BucketManager;
+
 public class BucketTest {
+
+    public static void main(String[] args) {
+        BucketManager bm = new BucketManager();
+
+        java.util.Random rand = new java.util.Random();
+
+        System.out.println("Stream started...");
+
+        for (int i = 0; i < 11; i++) {
+            double value = rand.nextGaussian();
+            bm.update(value);
+        }
+        System.out.println(bm.toString());
+        System.out.println("Stream finished...");
+
+    }
 }
